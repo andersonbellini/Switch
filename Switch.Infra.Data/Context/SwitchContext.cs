0c3be183401562bf83b7c984e7669d790bfbd928
Anderson Vinicius Bellini <andersonunip@gmail.com>
2018-10-20 14:47:26 -0300

    Configurando Classes do EF Core

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchDbContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public SwitchDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
